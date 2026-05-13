c17dc99fb4a0b0cad12da17f8c56030267c19be2
nasheii66 <ol.duncan@duocuc.cl>
2026-05-13 18:12:57 -0400

    Update ProductoCliente.java

--- ms-inventario/src/main/java/com/prueba/ms_inventario/cliente/ProductoCliente.java
+++ ms-inventario/src/main/java/com/prueba/ms_inventario/cliente/ProductoCliente.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// El nombre "ms-productos" debe coincidir con el nombre del servicio destino
 @FeignClient(name = "ms-productos", url = "http://localhost:8081")
 
 public interface ProductoCliente {
