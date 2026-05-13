9085020ff6c968248a059b40e5784b7f30411b33
PaoFranc <paolafranco.art@gmail.com>
2026-05-12 20:44:10 -0400

    primer commit

---
+++ ms-sucursales/src/main/java/com/prueba/ms_sucursales/MsSucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.ms_sucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSucursalesApplication.class, args);
+	}
+
+}
---
+++ ms-sucursales/src/test/java/com/prueba/ms_sucursales/MsSucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.ms_sucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSucursalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
