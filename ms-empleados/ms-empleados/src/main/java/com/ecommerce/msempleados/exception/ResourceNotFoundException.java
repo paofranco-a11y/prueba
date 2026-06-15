606fbdef3e52b3abc60e45d00e3824335d4147e1
MAlejandraRuiz <maa.ruiz@duocuc.cl>
2026-06-15 02:24:28 -0400

    ms-empleados funcional con java 17

--- ms-empleados/ms-empleados/src/main/java/com/ecommerce/msempleados/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.msempleados.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
