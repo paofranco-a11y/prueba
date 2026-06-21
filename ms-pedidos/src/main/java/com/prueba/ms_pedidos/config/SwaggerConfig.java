01a458e2ba64831b85d7e7ed9502d06e7429ec62
MAlejandraRuiz <maa.ruiz@duocuc.cl>
2026-06-21 16:56:27 -0400

    Actualización documentación ms-pedidos con swagger y HATEOAS

--- ms-pedidos/src/main/java/com/prueba/ms_pedidos/config/SwaggerConfig.java
+++ ms-pedidos/src/main/java/com/prueba/ms_pedidos/config/SwaggerConfig.java
@@ -1,19 +1,21 @@
-package com.prueba.ms_pedidos.config;
+package com.prueba.ms_pedidos.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Le dice a Spring Boot que cargue esta configuración al arrancar
 @Configuration
 public class SwaggerConfig {
 
+    // Configura los textos que saldrán en la página web de Swagger
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API 2026 PEDIDOS E-COMMERCE")
                         .version("1.0")
-                        .description("DOCUMENTACIÓN DE LA API PARA EL SISTEMA DE PEDIDOS ELECTRONICOS"));
+                        .description("DOCUMENTACIÓN DE LA API PARA EL CONTROL Y PROCESAMIENTO DE PEDIDOS"));
     }
 }
